c9591b09ec723cf7523f4e6fd4494111db1d9cbf
LeonGeroge <leongeroge@gmail.com>
2021-12-03 00:03:39 +0800

    batch listen

--- src/main/java/com/example/kafkaspringpractice/KafkaConfig.java
+++ src/main/java/com/example/kafkaspringpractice/KafkaConfig.java
@@ -1,21 +1,20 @@
 package com.example.kafkaspringpractice;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
 
 @Configuration
 @EnableKafka
 public class KafkaConfig {
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<?, ?> batchFactory(ConsumerFactory<Object, Object> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<Object, Object> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        factory.setBatchListener(true);  // <<<<<<<<<<<<<<<<<<<<<<<<<
-        factory.setConcurrency(10);
-        return factory;
-    }
+//    @Bean  // boot already provided
+//    public ConcurrentKafkaListenerContainerFactory<?, ?> batchFactory(ConsumerFactory<Object, Object> consumerFactory) {
+//        ConcurrentKafkaListenerContainerFactory<Object, Object> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory);
+//        factory.setBatchListener(true);  // <<<<<<<<<<<<<<<<<<<<<<<<<
+//        factory.setConcurrency(10);
+//        return factory;
+//    }
+//
+
 }
