8834961309b036e8a749919ecdac773f69b8738b
LeonGeroge <leongeroge@gmail.com>
2021-12-02 23:34:21 +0800

    init

---
+++ src/main/java/com/example/kafkaspringpractice/KafkaSpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringPracticeApplication.class, args);
+    }
+
+}
